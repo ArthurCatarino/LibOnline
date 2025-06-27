1a0376e9195a24cce5f9e71f8e543627daccd2ac
Petter <petterdouglas7@gmail.com>
2025-06-27 19:02:09 -0300

    refactor: atualização das rotas

--- Frontend/LibOnline/src/App.jsx
+++ Frontend/LibOnline/src/App.jsx
@@ -6,25 +6,15 @@ import Register from "./pages/Register";
 // Página não encontrada
 import NotFound from "./pages/NotFound";
 
-// Layouts de Usuário
-import LeitorLayout from "./pages/LeitorLayout";
-import BibliotecarioLayout from "./pages/BibliotecarioLayout";
-import AdminLayout from "./pages/AdminLayout";
-
 // Páginas do Leitor
 import EmprestimosLeitor from "./pages/leitor/Emprestimos";
 import SituacaoLeitor from "./pages/leitor/Situacao";
-import PagamentoLeitor from "./pages/leitor/Pagamento";
 
 // Páginas do Bibliotecário
 import TabelaLeitores from "./pages/bibliotecario/TabelaLeitores";
 import PerfilLeitor from "./pages/bibliotecario/PerfilLeitor";
 import TabelaLivros from "./pages/bibliotecario/TabelaLivros";
 import TabelaExemplares from "./pages/bibliotecario/TabelaExemplares";
-
-// Páginas do Admin
-import TabelaUsuarios from "./pages/admin/TabelaUsuarios";
-import PerfilUsuario from "./pages/admin/PerfilUsuario";
 
 function App() {
   return (
@@ -35,29 +25,18 @@ function App() {
 
         <Route path="/" element={<Login />} />
 
-        <Route path="/leitor/:leitorId" element={<LeitorLayout />}>
+        <Route path="/leitores" element={<TabelaLeitores />} />
+
+        <Route path="/leitor/:leitorId" element={<PerfilLeitor />}>
           <Route path="emprestimos" element={<EmprestimosLeitor />} />
           <Route path="situacao" element={<SituacaoLeitor />} />
-          <Route path="pagamento" element={<PagamentoLeitor />} />
         </Route>
-
+        
+        <Route path="/livros" element={<TabelaLivros />} />
         <Route
-          path="/bibliotecario/:bibliotecarioId"
-          element={<BibliotecarioLayout />}
-        >
-          <Route path="tabela-leitores" element={<TabelaLeitores />} />
-          <Route path="leitor/:leitorId" element={<PerfilLeitor />} />
-          <Route path="tabela-livros" element={<TabelaLivros />} />
-          <Route
-            path="livro/:livroId/tabela-exemplares"
-            element={<TabelaExemplares />}
-          />
-        </Route>
-
-        <Route path="/admin/:adminId" element={<AdminLayout />}>
-          <Route path="usuarios" element={<TabelaUsuarios />} />
-          <Route path="usuario/:usuarioId" element={<PerfilUsuario />} />
-        </Route>
+          path="/livro/:livroId/tabela-exemplares"
+          element={<TabelaExemplares />}
+        />
 
         <Route path="*" element={<NotFound />} />
       </Routes>
